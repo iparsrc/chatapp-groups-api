b080b4783bfcd2d86051719237aa00e429f53282
parsa <akbariparsa1209@gmail.com>
2020-08-11 21:41:40 +0430

    Added Create func.

---
+++ domain/dao.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	"github.com/parsaakbari1209/ChatApp-groups-api/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func Create(group *Group) (*Group, *utils.RestErr) {
+	usersC := db.Collection("groups")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	result, err := usersC.InsertOne(ctx, bson.M{
+		"name":            group.Name,
+		"owner":           group.Owner,
+		"admins":          group.Admins,
+		"members":         group.Members,
+		"private":         group.Private,
+		"dateCreated":     group.DateCreated,
+		"description":     group.Description,
+		"groupUniqueName": group.GroupUniqueName,
+	})
+	if err != nil {
+		return nil, utils.BadRequest("can't create user.")
+	}
+	group.ID = result.InsertedID.(primitive.ObjectID)
+	return group, nil
+}
