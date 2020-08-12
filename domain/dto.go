75116d6aae47cdae8eeaba6448c8475832ffec5b
parsa <akbariparsa1209@gmail.com>
2020-08-12 19:13:43 +0430

    Deleted groupUniqueName.

--- domain/dto.go
+++ domain/dto.go
@@ -1,17 +1,13 @@
 package domain
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Group struct {
-	ID              primitive.ObjectID `json:"id" bson:"_id"`
-	Name            string             `json:"name" bson:"name"`
-	Owner           string             `json:"owner" bson:"owner"`
-	Admins          []string           `json:"admins" bson:"admins"`
-	Members         []string           `json:"members" bson:"members"`
-	Private         bool               `json:"private" bson:"private"`
-	DateCreated     int64              `json:"dateCreated" bson:"dateCreated"`
-	Description     string             `json:"description" bson:"description"`
-	GroupUniqueName string             `json:"groupUniqueName" bson:"groupUniqueName"`
+	// ID must be a unique name reqresenting group.
+	ID          string   `json:"id" bson:"_id"`
+	Name        string   `json:"name" bson:"name"`
+	Owner       string   `json:"owner" bson:"owner"`
+	Admins      []string `json:"admins" bson:"admins"`
+	Members     []string `json:"members" bson:"members"`
+	Private     bool     `json:"private" bson:"private"`
+	DateCreated int64    `json:"dateCreated" bson:"dateCreated"`
+	Description string   `json:"description" bson:"description"`
 }
