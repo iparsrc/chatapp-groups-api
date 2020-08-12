0aefcb6a02289ad9e94c30b2504831649e14a0da
parsa <akbariparsa1209@gmail.com>
2020-08-12 19:14:13 +0430

    Deleted groupUniqueName releated lines.

--- domain/dao.go
+++ domain/dao.go
@@ -6,26 +6,24 @@ import (
 
 	"github.com/parsaakbari1209/ChatApp-groups-api/utils"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func Create(group *Group) (*Group, *utils.RestErr) {
 	groupsC := db.Collection("groups")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
-	result, err := groupsC.InsertOne(ctx, bson.M{
-		"name":            group.Name,
-		"owner":           group.Owner,
-		"admins":          group.Admins,
-		"members":         group.Members,
-		"private":         group.Private,
-		"dateCreated":     group.DateCreated,
-		"description":     group.Description,
-		"groupUniqueName": group.GroupUniqueName,
+	_, err := groupsC.InsertOne(ctx, bson.M{
+		"_id":         group.ID,
+		"name":        group.Name,
+		"owner":       group.Owner,
+		"admins":      group.Admins,
+		"members":     group.Members,
+		"private":     group.Private,
+		"dateCreated": group.DateCreated,
+		"description": group.Description,
 	})
 	if err != nil {
 		return nil, utils.BadRequest("can't create user.")
 	}
-	group.ID = result.InsertedID.(primitive.ObjectID)
 	return group, nil
 }
