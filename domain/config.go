3fc006b5df73719edca4dc03b971b2f1ba7b6089
parsa <akbariparsa1209@gmail.com>
2020-08-12 21:16:32 +0430

    Deleted two CheckForIndexes and CreateIndex funcs.

--- domain/config.go
+++ domain/config.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -29,54 +27,4 @@ func ConnectDB(uri string) {
 	}
 	// db can be used in package level.
 	db = client.Database("groups")
-	// check for indexes.
-	CheckForIndexes()
-}
-
-func CheckForIndexes() {
-	groupC := db.Collection("groups")
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	cur, err := groupC.Indexes().List(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cur.Close(ctx)
-
-	ctxx, cancelx := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancelx()
-	for cur.Next(ctxx) {
-		var index bson.M
-		if err := cur.Decode(&index); err != nil {
-			log.Fatal(nil)
-		}
-		if index["name"] == "groupUniqueName" {
-			return
-		}
-	}
-	// At this point we are sure that groupname is not an index in the collection.
-	CreateIndex("groupUniqueName", true)
-}
-
-func CreateIndex(name string, unique bool) {
-	groupC := db.Collection("groups")
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	ops := options.IndexOptions{
-		Name:   &name,
-		Unique: &unique,
-	}
-	m := mongo.IndexModel{
-		Keys:    bson.D{{Key: name, Value: 1}},
-		Options: &ops,
-	}
-	n, err := groupC.Indexes().CreateOne(ctx, m)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if n != name {
-		log.Fatal("index name is not groupUniqueName.")
-	}
 }
