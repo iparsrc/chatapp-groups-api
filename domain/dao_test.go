8408ac5ef7c7d8cc1b5fd015dbdb7fe97263d123
parsa <akbariparsa1209@gmail.com>
2020-08-12 21:23:45 +0430

    Added TestCreate func.

---
+++ domain/dao_test.go
@@ -0,0 +1,39 @@
+package domain
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCreate(t *testing.T) {
+	ConnectDB("mongodb://localhost:27017")
+	groupOne := Group{
+		ID:          "A",
+		Name:        "LetsPair",
+		Owner:       "1",
+		Members:     []string{"1"},
+		Private:     true,
+		DateCreated: time.Now().Unix(),
+		Description: "Hey guys lets pair together.",
+	}
+	groupTwo := Group{
+		ID:          "B",
+		Name:        "GoLang-Gang",
+		Owner:       "2",
+		Members:     []string{"2", "1"},
+		Admins:      []string{"1"},
+		Private:     false,
+		DateCreated: time.Now().Unix(),
+		Description: "This group is for golang gang yeah.",
+	}
+	group, restErr := Create(&groupOne)
+	if restErr != nil {
+		t.Error(restErr.Message)
+	}
+	println("    ", group)
+	group, restErr = Create(&groupTwo)
+	if restErr != nil {
+		t.Error(restErr.Message)
+	}
+	println("    ", group)
+}
